919618a91da0f9002b384e26d5f716513bf60ad7
ducdt <ducdt@react-plus.com>
2022-06-27 11:37:28 +0700

    first commit

---
+++ src/cats/dto/create-cat.dto.ts
@@ -0,0 +1,19 @@
+import { IsEmpty, IsNotEmpty, ValidationArguments } from 'class-validator';
+export class CreateCatDto {
+  @IsNotEmpty({
+    message: (e: ValidationArguments) => {
+      console.log(e);
+
+      return `${e.property} Không được để trống`;
+    },
+  })
+  id: string;
+  @IsNotEmpty({
+    message: (e: ValidationArguments) => {
+      console.log(e);
+
+      return `${e.property} Không được để trống`;
+    },
+  })
+  name: string;
+}
